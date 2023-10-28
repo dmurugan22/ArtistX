1fb49977f2ef41990eb69f56ff273d358852a4dc
Divyesh Murugan <divyeshmurugan@gmail.com>
2023-10-28 14:31:44 -0700

    Started Dex

--- src/xrpl tutorial/dex.js
+++ src/xrpl tutorial/dex.js
@@ -8,7 +8,13 @@ async function main() {
     const client = new xrpl.Client("wss://s.altnet.rippletest.net:51233")
     await client.connect()
 
-    // ... custom code goes here
+    // Get credentials from the Testnet Faucet -----------------------------------
+    console.log("Requesting address from the Testnet faucet...")
+    const wallet = (await client.fundWallet()).wallet
+    console.log(`Got address ${wallet.address}.`)
+    // To use existing credentials, you can load them from a seed value, for
+    // example using an environment variable as follows:
+    // const wallet = xrpl.Wallet.fromSeed(process.env['MY_SEED'])
 
     // Disconnect when done (If you omit this, Node.js won't end the process)
     await client.disconnect()
