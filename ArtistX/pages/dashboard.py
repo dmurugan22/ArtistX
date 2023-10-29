b7959fc76e94c85e745191fb95006fcf2b28fdf7
Aaryan Chandna <chandna@berkeley.edu>
2023-10-29 12:10:55 -0700

    done

--- ArtistX/pages/dashboard.py
+++ ArtistX/pages/dashboard.py
@@ -5,32 +5,27 @@ from ArtistX.state import State
 import reflex as rx
 
 
-# Sample data (this would typically come from your backend/database)
 wallet = [
     {"name": "Drakecoin", "symbol": "DRK", "price": State.prices['Drake'], "quantity": State.wallet['Drake'], "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAB78TvJChoaYVo_20-U39wygzmZnBFr0lfLQwwfjSTg&s"},
     {"name": "Kanyethereum", "symbol": "KAN", "price": State.prices['Kanye'], "quantity": State.wallet['Kanye'], "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT-XlOz7n7DnAPOFD3gyMXajoCMpq2LozP5ZSyHJU&s"},
     {"name": "TayloRP", "symbol": "TAY", "price": State.prices['Taylor'], "quantity": State.wallet['Taylor'], "url": "http://t1.gstatic.com/images?q=tbn:ANd9GcTIThZ_9MimtX7eh_QJ9waeguPw2Gh0rpzk4FY5JvdZSXPSvC_Aqjt8u8dsOWEVXd-V3PNJ"},
-    {"name": "Beyoncécoin", "symbol": "BEY", "price": State.prices['Beyoncé'], "quantity": State.wallet['Beyoncé'], "url": "https://example.com/beyonce-image-url"},
-    {"name": "Edcoin", "symbol": "EDS", "price": State.prices['Ed Sheeran'], "quantity": State.wallet['Ed Sheeran'], "url": "https://example.com/ed-sheeran-image-url"},
-    {"name": "ArianaCoin", "symbol": "ARG", "price": State.prices['Ariana Grande'], "quantity": State.wallet['Ariana Grande'], "url": "https://example.com/ariana-grande-image-url"},
-    {"name": "Biebercoin", "symbol": "JUB", "price": State.prices['Justin Bieber'], "quantity": State.wallet['Justin Bieber'], "url": "https://example.com/justin-bieber-image-url"},
-    {"name": "Rihannacoin", "symbol": "RIH", "price": State.prices['Rihanna'], "quantity": State.wallet['Rihanna'], "url": "https://example.com/rihanna-image-url"},
-    {"name": "Adelecoin", "symbol": "ADE", "price": State.prices['Adele'], "quantity": State.wallet['Adele'], "url": "https://example.com/adele-image-url"},
-    {"name": "Shawncoin", "symbol": "SHM", "price": State.prices['Shawn Mendes'], "quantity": State.wallet['Shawn Mendes'], "url": "https://example.com/shawn-mendes-image-url"},
-    # Add more cryptocurrencies as needed
+    {"name": "Beyoncécoin", "symbol": "BEY", "price": State.prices['Beyonce'], "quantity": State.wallet['Beyonce'], "url": "https://cdn.britannica.com/51/188751-050-D4E1CFBC/Beyonce-2010.jpg"},
+    {"name": "Edcoin", "symbol": "EDS", "price": State.prices['Ed'], "quantity": State.wallet['Ed'], "url": "https://cdn.britannica.com/17/249617-050-4575AB4C/Ed-Sheeran-performs-Rockefeller-Plaza-Today-Show-New-York-2023.jpg"},
+    {"name": "ArianaCoin", "symbol": "ARG", "price": State.prices['Ariana'], "quantity": State.wallet['Ariana'], "url": "https://www.billboard.com/wp-content/uploads/2023/03/Ariana-Grande-2020-billboard-1548.jpg?w=942&h=623&crop=1"},
+    {"name": "Biebercoin", "symbol": "JUB", "price": State.prices['Justin'], "quantity": State.wallet['Justin'], "url": "https://m.media-amazon.com/images/M/MV5BMjE1NjMxMDUyM15BMl5BanBnXkFtZTgwODMzNDM1NTE@._V1_.jpg"},
+    {"name": "Rihannacoin", "symbol": "RIH", "price": State.prices['Rihanna'], "quantity": State.wallet['Rihanna'], "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Rihanna_Fenty_2018.png/640px-Rihanna_Fenty_2018.png"},
+    {"name": "Adelecoin", "symbol": "ADE", "price": State.prices['Adele'], "quantity": State.wallet['Adele'], "url": "https://upload.wikimedia.org/wikipedia/commons/5/52/Adele_for_Vogue_in_2021.png"},
+    {"name": "Shawncoin", "symbol": "SHM", "price": State.prices['Shawn'], "quantity": State.wallet['Shawn'], "url": "https://upload.wikimedia.org/wikipedia/commons/a/a4/191125_Shawn_Mendes_at_the_2019_American_Music_Awards.png"},
 ]
 
 
-# Sample balance (this would also come from your backend/database)
 balance = float(5000.00)  # in USD
 
 
 @template(route="/dashboard", title="Dashboard")
 def dashboard():
-    # Calculate total value of all cryptocurrencies in USD
     total_value = sum([coin_data["quantity"] * coin_data["price"] for coin_data in wallet])
     
-    # Create a list of components for each cryptocurrency
     crypto_components = []
     for data in wallet:
         crypto_value = data["quantity"] * data["price"]
--- src/api.py
+++ src/api.py
@@ -9,9 +9,10 @@ async def customer_transaction_sell(wallet, currency, amount, cost):
     client = xrpl.clients.JsonRpcClient(testnet_url)
     cold_wallet = generate_faucet_wallet(client, debug=True)
     hot_wallet = generate_faucet_wallet(client, debug=True)
-
+    seq = [0]
     def submit_transaction(tx, wallet):
         response = xrpl.transaction.submit_and_wait(tx, client, wallet)
+        seq[0]= response.result['LastLedgerSequence']
         if 'validated' in response.result and response.result['validated']:
             print(f"Transaction successful")
             return True
@@ -80,6 +81,7 @@ async def customer_transaction_sell(wallet, currency, amount, cost):
 
     issue_quantity = amount
     send_token_tx = xrpl.models.transactions.Payment(
+        last_ledger_sequence=seq[0] + 1000,
         account=wallet.address,
         destination=hot_wallet.address,
         amount=xrpl.models.IssuedCurrencyAmount(
@@ -108,15 +110,17 @@ async def customer_transaction_sell(wallet, currency, amount, cost):
     response =  xrpl.transaction.submit_and_wait(trust_set_tx, client, wallet)
     print(response)
     """
-
+    """
     issue_quantity = cost
     payment = xrpl.models.transactions.Payment(
+        last_ledger_sequence=seq[0] + 1000,
         account=cold_wallet.address,
         destination=wallet.address,
         amount=xrpl.utils.xrp_to_drops(int(cost))
     )
     if not submit_transaction(payment, cold_wallet): 
         return False
+    """
     
     return True
 
@@ -127,9 +131,10 @@ async def customer_transaction_buy(wallet, currency, amount, cost):
     client = xrpl.clients.JsonRpcClient(testnet_url)
     cold_wallet = generate_faucet_wallet(client, debug=True)
     hot_wallet = generate_faucet_wallet(client, debug=True)
-
+    seq = 0
     def submit_transaction(tx, wallet):
         response = xrpl.transaction.submit_and_wait(tx, client, wallet)
+        seq[0] = response.result['LastLedgerSequence']
         if 'validated' in response.result and response.result['validated']:
             print(f"Transaction successful")
             return True
@@ -199,6 +204,7 @@ async def customer_transaction_buy(wallet, currency, amount, cost):
 
     issue_quantity = amount
     send_token_tx = xrpl.models.transactions.Payment(
+        last_ledger_sequence=seq[0] + 1000,
         account=cold_wallet.address,
         destination=wallet.address,
         amount=xrpl.models.IssuedCurrencyAmount(
@@ -210,9 +216,10 @@ async def customer_transaction_buy(wallet, currency, amount, cost):
 
     if not submit_transaction(send_token_tx, cold_wallet):
         return False
-
+    """
     issue_quantity = cost
     payment = xrpl.models.transactions.Payment(
+        last_ledger_sequence=seq[0]+1000,
         account=wallet.address,
         destination=hot_wallet.address,
         amount=xrpl.utils.xrp_to_drops(int(cost))
@@ -220,5 +227,6 @@ async def customer_transaction_buy(wallet, currency, amount, cost):
 
     if not submit_transaction(payment, wallet):
         return False
+    """
 
     return True
